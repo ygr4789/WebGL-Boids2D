c38731fe01de37eafe5639875343b718e936a2b7
ygr4789 <yongdori@snu.ac.kr>
2023-03-23 08:49:30 +0900

    rule 1~3

--- src/main.js
+++ src/main.js
@@ -11,14 +11,20 @@ function main() {
 
   let objects = [];
   let numObjects = 100;
-  let meanX, meanY;
-  let avoidDist = 100;
+  let avoidDistance = 20;
+  // let visualRange = 20;
+  let margin = 100;
+  let factor1 = 0.005;
+  let factor3 = 0.05;
+  let factor2 = 0.05;
+  let vlim = 500;
+  let Xmin, Xmax, Ymin, Ymax;
 
   function object() {
     this.x = Math.random() * gl.canvas.width;
     this.y = Math.random() * gl.canvas.height;
-    this.vx = Math.random() * 100;
-    this.vy = Math.random() * 100;
+    this.vy = (Math.random() - 0.5) * 100000;
+    this.vx = (Math.random() - 0.5) * 100000;
     this.color = [Math.random(), Math.random(), Math.random(), 1];
     this.update = function (dt) {
       this.x += this.vx * dt;
@@ -32,30 +38,61 @@ function main() {
     }
   }
 
-  function calcMeanPosition() {
-    meanX = 0;
-    meanY = 0;
-    for (let i = 0; i < numObjects; i++) {
-      meanX += objects[i].x / numObjects;
-      meanY += objects[i].y / numObjects;
-    }
-  }
-  
   function distOfBoids(b1, b2) {
-    return Math.sqrt(Math.pow(b1.x-b2.x, 2) + Math.pow(b1.y-b2.y, 2));
+    return Math.sqrt(Math.pow(b1.x - b2.x, 2) + Math.pow(b1.y - b2.y, 2));
+  }
+  function normOfVector(x, y) {
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  }
+  function updateBound() {
+    Xmin = Ymin = margin;
+    Xmax = gl.canvas.width - margin;
+    Ymax = gl.canvas.height - margin;
   }
 
   function updateBoidsVelocity() {
     for (let i = 0; i < numObjects; i++) {
-      objects[i].vx += (meanX - objects[i].x) / numObjects;
-      objects[i].vy += (meanY - objects[i].y) / numObjects;
-    }
-    for (let i = 0; i < numObjects; i++) {
+      var ax1, ax2, ax3;
+      var ay1, ay2, ay3;
+
+      let meanPosX = 0;
+      let meanPosY = 0;
+      let meanVelX = 0;
+      let meanVelY = 0;
+      for (let j = 0; j < numObjects; j++) {
+        meanPosX += objects[j].x / numObjects;
+        meanPosY += objects[j].y / numObjects;
+        meanVelX += objects[j].vx / numObjects;
+        meanVelY += objects[j].vy / numObjects;
+      }
+
+      ax1 = (meanPosX - objects[i].x) * factor1;
+      ay1 = (meanPosY - objects[i].y) * factor1;
+      ax3 = (meanVelX - objects[i].vx) * factor3;
+      ay3 = (meanVelY - objects[i].vy) * factor3;
+      ax2 = ay2 = 0;
       for (let j = 0; j < numObjects; j++) {
         if (i == j) continue;
-        if(distOfBoids(objects[i], objects[j]) < avoidDist) continue;
-        
+        if (distOfBoids(objects[i], objects[j]) > avoidDistance) continue;
+        var dx = objects[j].x - objects[i].x;
+        var dy = objects[j].y - objects[i].y;
+        ax2 -= dx * factor2;
+        ay2 -= dy * factor2;
       }
+
+      objects[i].vx += ax1 + ax2 + ax3;
+      objects[i].vy += ay1 + ay2 + ay3;
+
+      var vnorm = normOfVector(objects[i].vx, objects[i].vy);
+      if (vnorm > vlim) {
+        objects[i].vx = (objects[i].vx / vnorm) * vlim;
+        objects[i].vy = (objects[i].vy / vnorm) * vlim;
+      }
+
+      if (objects[i].x < Xmin) objects[i].vx += 1;
+      else if (objects[i].x > Xmax) objects[i].vx -= 1;
+      else if (objects[i].y < Ymin) objects[i].vy += 1;
+      else if (objects[i].y > Ymax) objects[i].vy -= 1;
     }
   }
 
@@ -65,6 +102,7 @@ function main() {
     }
   }
 
+  updateBound();
   initBoids();
   // setup GLSL program
   var program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-2d", "fragment-shader-2d"]);
@@ -125,7 +163,7 @@ function main() {
       gl.drawArrays(primitiveType, offset, count);
     }
 
-    calcMeanPosition();
+    updateBound();
     updateBoidsVelocity();
     updateBoidsPosition(timeDiff);
     requestAnimationFrame(drawScene);
